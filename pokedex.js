d4d329ce954f2c50fe40a71e5aabb58d4c5b9150
martscribeLaptop <martscribe@gmail.com>
2023-01-31 13:20:48 -0600

    build pokedex api request page Include a search bar to search for pokemon Sprite and name of pokemon Ability of pokemon not found page

---
+++ pokedex.js
@@ -0,0 +1,36 @@
+const fetchPokemon = async () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    let data = await fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("./pokemon-sad.gif")
+        }
+        else {
+            return res.json();
+        }
+    })
+    if (data) {
+            console.log(data);
+            let pokeImg = data.sprites.front_default;
+            let pokeInfo = data.abilities
+            pokeImage(pokeImg);
+            pokeData(pokeInfo)
+            console.log(pokeInfo);
+    };
+}
+
+const pokeImage = (url) => {
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
+
+const pokeData = (abilities) => {
+    const pokeAbilities = document.getElementById("abilities")
+    const abilitiesName = abilities.map((item) => item.ability.name)
+    //recorres todo el objeto y te retorna un nuevo arreglo segun lo que le pides
+    pokeAbilities.innerHTML = "<p>" + abilitiesName + "</p>";
+
+}
